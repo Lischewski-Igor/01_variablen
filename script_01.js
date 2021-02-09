39aefb9d6ae68b58c98eaba7fb968ea117d75fba
Igor <igor.lischewski@web.de>
2021-02-09 10:36:01 +0100

    Variablen Typen

--- script_01.js
+++ script_01.js
@@ -27,10 +27,63 @@ console.log("Hallo, " + firstName + " " + familyName + "!");
 */
 
 // JS ist eine untypisierte Sprache! | untyped
+/*
 let test;  // Deklaration
-test = "hi";  // String
+test = "hi";  // string
 test = 2; // Number
 test = true; // Boolean
 
 console.log("Typ: " + typeof test);
 console.log("Inhalt: " + test);
+*/
+
+/***** Variablen 02 *******/
+
+// Deklaration
+/*
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark, now;
+
+// Wertzuweisung | Assigment
+let date = new Date();
+now = date.getFullYear();
+ageJohn = 25;
+ageMark = 30;
+
+// Operation
+birthYearJohn = now - ageJohn;
+birthYearMark = now - ageMark;
+
+// Ausgabe
+console.log("Aktuelles Jahr: " + now);
+console.log("birthYearJohn: " + birthYearJohn);
+console.log("birthYearMark: " + birthYearMark);
+console.log("------------------------");
+*/
+
+/***** Variablen 03 *******/
+// Deklaration | Wertzuweisung
+/*
+let ageJohn, ageMark;
+ageJohn = 35;
+ageMark = 30;
+
+// Operation (Vergleich/Test)
+let isJohnOlder = (ageJohn > ageMark);
+
+console.log(isJohnOlder);
+*/
+
+/***** Variablen 04 *******/
+// Zusammenfassung Datentypen
+
+let nix;
+let ageJohn = 28;
+let ageMark = 30;
+let firstName = "John";
+let isJohnOlder = (ageJohn > ageMark);
+
+console.log("nix: " + typeof nix);
+console.log("ageJohn: " + typeof ageJohn);
+console.log("firstName: " + typeof firstName);
+console.log("isJohnOlder: " + typeof isJohnOlder);
